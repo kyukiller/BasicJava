1407d22238a418f0d64adfecd363597c9da15152
kyukiller <kyukiller@gmail.com>
2016-08-19 16:32:59 +0900

    Basic Library in Java

---
+++ test/org/totalbeginner/tutorial/PersonTest.java
@@ -0,0 +1,33 @@
+package org.totalbeginner.tutorial;
+
+import junit.framework.TestCase;
+
+public class PersonTest extends TestCase {
+
+	public void testPerson() {
+		Person p1 = new Person();
+		assertEquals("unknown name", p1.getName());
+		assertEquals(3, p1.getMaximumBooks());
+	}
+
+	public void testSetName() {
+		Person p2 = new Person();
+		p2.setName("Q");
+		assertEquals("Q", p2.getName());
+	}
+
+	public void testSetMaximumBooks() {
+		Person p3 = new Person();
+		p3.setMaximumBooks(3);
+		assertEquals(3, p3.getMaximumBooks());
+	}
+	
+	public void testToString() {
+		Person p4 = new Person();
+		p4.setName("Fred Flintstone");
+		p4.setMaximumBooks(7);
+		String testString = "Fred Flintstone (7 books)";
+		assertEquals(testString, p4.toString());
+	}
+
+}
